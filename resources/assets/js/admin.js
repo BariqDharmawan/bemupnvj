9baa38209b1be0dc59e148df3828637164b271f3
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-08-22 10:47:40 +0700

    [UPDATE] frontend asset

--- resources/assets/js/admin.js
+++
@@ -1,3 +0,0 @@
-import './../scss/admin.scss';
-
-require('admin-lte');
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,8 +3,15 @@ let mix = require('laravel-mix');
 mix.setResourceRoot('/assets/');
 mix.setPublicPath('public/assets/');
 
-mix.sass('resources/assets/scss/admin.scss', 'css')
-    .js('resources/assets/js/admin.js', 'js')
+mix.styles([
+    'public/assets/AdminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
+    'public/assets/AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
+    'public/assets/AdminLTE/plugins/jqvmap/jqvmap.min.css',
+    'public/assets/AdminLTE/dist/css/adminlte.min.css',
+    'public/assets/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css',
+    'public/assets/AdminLTE/plugins/daterangepicker/daterangepicker.css',
+    'public/assets/AdminLTE/plugins/summernote/summernote-bs4.min.css'
+], 'public/assets/css/admin.css')
 
 mix.sass('resources/assets/scss/app.scss', 'public/assets/css')
     .js('resources/assets/js/app.js', 'public/assets/js')
