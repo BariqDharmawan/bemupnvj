22476719ef5b7e234fb0a0018d2557d5300c92f4
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-12-06 20:39:07 +0700

    [FIX] pdf

--- resources/assets/js/function/pdf.js
+++ resources/assets/js/function/pdf.js
@@ -4,6 +4,5 @@ $node.each(function (index, pdfEl) {
   const pdfSrc = pdfEl.dataset.pdfSrc
   const pdfId = pdfEl.getAttribute('id')
 
-  console.log(pdfSrc, pdfId)
   PDFObject.embed(pdfSrc, `#${pdfId}`);
 })
