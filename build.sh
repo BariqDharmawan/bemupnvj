3edbb3a9ca2fd1308c34d0501b62390e67046104
uloydev <uloydev@gmail.com>
2021-12-22 00:06:16 +0700

    add build script for production

---
+++ build.sh
@@ -0,0 +1,10 @@
+echo "start building backend"
+yarn build
+echo "start building frontend
+yarn production
+echo "copy .env to build folder"
+cp .env build/.env
+cd build
+echo "install backend dependencies"
+yarn install --production
+echo "its finish, and ready to run"
