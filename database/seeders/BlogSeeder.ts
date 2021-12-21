bd839fd2d93c8a78afe90557477281f855e95585
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-12-21 23:14:32 +0700

    [UPDATE]  remove development only

--- database/seeders/BlogSeeder.ts
+++ database/seeders/BlogSeeder.ts
@@ -2,8 +2,6 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import { BlogCategoriesFactories } from 'Database/factories/blogs_factories'
 
 export default class BlogSeeder extends BaseSeeder {
-    public static developmentOnly = true
-
     public async run() {
         await BlogCategoriesFactories.with(
             'blogs', 1, (blog) => blog.apply('isLatestNews')
--- database/seeders/FacultyAndMajorSeeder.ts
+++ database/seeders/FacultyAndMajorSeeder.ts
@@ -2,8 +2,6 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import { FacultyFactories } from 'Database/factories/faculty_and_major_factories'
 
 export default class FacultyAndMajorSeeder extends BaseSeeder {
-    public static developmentOnly = true
-
     public async run() {
         await FacultyFactories.with('majors', 3).createMany(5)
     }
